d0bb160881d425b7e4900b47eebb9490727fea72
Kuc-kuc-kuc-kuc <scheiermartin7@gmail.com>
2024-02-14 13:44:03 +0100

    Takarítás

--- scripts/docker/docker-build.js
+++
@@ -1,7 +0,0 @@
-const version = require('../version.js');
-const sh = require('shelljs');
-
-const imageName = 'sb-clean-blog-angular';
-
-sh.exec(`docker build -t ${imageName}:latest -t ${imageName}:${version} .`);
-
